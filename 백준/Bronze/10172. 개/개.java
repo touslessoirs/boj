61abe6771a6fb2518ba3865c0147cdce855d1325
touslessoirs <111061201+touslessoirs@users.noreply.github.com>
2022-08-17 10:28:24 +0900

    [Bronze V] Title: 개, Time: 164 ms, Memory: 16204 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        char ch = '"';
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )"+ch+ch+ch+"\\");
+        System.out.println("|"+ch+"^"+ch+"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}

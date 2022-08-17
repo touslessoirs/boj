64d1b87a95bfb04674abf63ba468f29a8b87e31a
touslessoirs <111061201+touslessoirs@users.noreply.github.com>
2022-08-17 10:48:16 +0900

    [Bronze V] Title: 새싹, Time: 136 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        char ch = '"';
+		System.out.println("         ,r'"+ch+"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. "+ch+". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+    }
+}

8c3642759935bc9db6fab33a0a0fc2bbff1b2040
touslessoirs <111061201+touslessoirs@users.noreply.github.com>
2022-08-18 11:11:18 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 372 ms, Memory: 20380 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        for(int i=1;i<=num;i++){
+        	for(int b=1;b<i;b++) {
+        		System.out.print(" ");
+        	}
+            for(int s=0;s<=num-i;s++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

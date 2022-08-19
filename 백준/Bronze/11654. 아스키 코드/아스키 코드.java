f7e06469e5278a0fcad17b8455cafa8ba118c8f8
touslessoirs <111061201+touslessoirs@users.noreply.github.com>
2022-08-19 16:31:22 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17528 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int ch = sc.next().charAt(0);
+		
+		System.out.println(ch);
+		
+	}
+
+}

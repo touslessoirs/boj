d6a7152fb715b70a37985124791b0e3454216a2b
touslessoirs <111061201+touslessoirs@users.noreply.github.com>
2022-08-25 11:37:46 +0900

    [Bronze V] Title: A+B - 3, Time: 228 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int total = sc.nextInt();
+		int num1[] = new int[total];
+		int num2[] = new int[total];
+		
+		for(int i=0;i<total;i++) {
+			num1[i] = sc.nextInt();
+			num2[i] = sc.nextInt();
+		}
+		
+		for(int i=0;i<total;i++) {
+			System.out.println(num1[i]+num2[i]);
+		}
+        
+	}
+}

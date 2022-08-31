5403aa4b0bcc5f0c14e6f600ea959a5104a0091d
touslessoirs <111061201+touslessoirs@users.noreply.github.com>
2022-08-31 23:00:26 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 18036 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double num1 = sc.nextDouble();
+		double num2 = sc.nextDouble();
+		
+		System.out.println(num1/num2);
+	}
+
+}

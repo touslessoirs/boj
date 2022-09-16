c0595b4706e8aae873aad861e27b2ee3a1316168
touslessoirs <111061201+touslessoirs@users.noreply.github.com>
2022-09-16 17:41:59 +0900

    [Bronze IV] Title: R2, Time: 204 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int r1 = sc.nextInt();
+		int num = sc.nextInt();
+		
+		int r2 = 2*num-r1;
+		System.out.println(r2);
+
+	}
+}

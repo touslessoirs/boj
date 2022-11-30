3098d0c913e31e9d2f776fbb40e2d34266fcf579
touslessoirs <111061201+touslessoirs@users.noreply.github.com>
2022-11-30 09:48:08 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int A = Integer.parseInt(sc.nextLine());
+		System.out.println(A-543);
+
+		sc.close();
+		
+	}
+	
+}

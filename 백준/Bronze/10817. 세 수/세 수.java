3e69beb2c868dd6e68feca64ecc18080fcd74eff
touslessoirs <111061201+touslessoirs@users.noreply.github.com>
2022-12-27 20:46:45 +0900

    [Bronze III] Title: 세 수, Time: 216 ms, Memory: 17856 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int arr[] = new int[3];
+
+		for(int i=0;i<3;i++) {
+			arr[i]=sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		System.out.println(arr[1]);
+		
+	}
+}
